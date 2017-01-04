19d241f3d0dfb89efd9e34331b8779b8553dad2b
Andrew Fan <andrewsuicune@gmail.com>
2017-01-04 13:58:04 -0500

    Added untested tech.lua and some locale stuff with descriptions.

---
+++ Sparen-PRT/prototypes/technology.lua
@@ -0,0 +1,45 @@
+data:extend({
+    {
+        type = "technology",
+        name = "prttech",
+        icon = "__Sparen-PRT__/graphics/icon_prt_tech.png",
+        effects = {
+            {
+                type = "unlock-recipe",
+                recipe = "prt-straight-rail"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = "prt-block-signal"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = "prt-chain-signal"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = "prt-bridge"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = "prt-pod"
+            }
+        },
+        prerequisites = {
+            "automated-rail-transportation", 
+            "electric-engine", 
+            "electric-energy-accumulators-1", 
+            "solar-energy"
+        },
+        unit = {
+            count = 200,
+            ingredients =
+            {
+                {"science-pack-1", 1},
+                {"science-pack-2", 1}
+            },
+            time = 20
+        },
+        order = "c-g-d",
+    }
+})
