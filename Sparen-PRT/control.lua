7170250618bf6ff06000c2290e3aecc5280a194d
Andrew Fan <andrewsuicune@gmail.com>
2017-01-04 14:04:32 -0500

    Began work on control.lua

---
+++ Sparen-PRT/control.lua
@@ -0,0 +1,20 @@
+require "util"
+require "defines"
+
+--Initialize global variables that are handled on loading a save file here.
+game.oninit(function() 
+
+    end
+)
+
+game.onevent(defines.events.ontick, function(event)
+    --If the player is in a PRT Pod, do XYZ
+    --if game.player.character and game.player.character.vehicle and game.player.character.vehicle.name == "prt-pod" then
+
+    --end
+
+    --Handle Pod Logic here
+    --TODO
+
+    end
+)
